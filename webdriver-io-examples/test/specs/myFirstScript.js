3cf45cc7d0c065b064a6d4daf9cfaa792d9303b4
Gaurav Khanna <gaurav.khanna@tiladmins-MacBook-Pro-22.local>
2020-04-17 21:21:29 +0530

    fixes

--- webdriver-io-examples/test/specs/myFirstScript.js
+++ webdriver-io-examples/test/specs/myFirstScript.js
@@ -1,13 +1,18 @@
 const assert = require("assert");
- 
+
 describe("WebDriver IO Demo", function() {
-    it("WebDriver IO TestCase", function() {
+
+    it("WebDriver IO TestCase 1", function() {
+
         browser.url("https://lambdatest.github.io/sample-todo-app/");
+
         $("*[name='li1']").click();
+
         $("*[name='li2']").click();
-       $("#sampletodotext").setValue("Lambdatest\n");
-        
+
+        $("#sampletodotext").setValue("Lambdatest\n");
+
         assert.strictEqual(browser.getTitle(), "Sample page - lambdatest.com");
-        
+
     });
 });
